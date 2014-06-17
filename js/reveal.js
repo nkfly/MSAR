615d90de2a880c5dc30953e915ace0dab1d45585
nkfly <nkflyfly@gmail.com>
2014-06-18 01:11:22 +0800

    demo pages

--- js/reveal.js
+++ js/reveal.js
@@ -19,21 +19,21 @@ var Reveal = (function(){
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
+			width: "100%",
 			height: 700,
 
 			// Factor of the display size that should remain empty around the content
-			margin: 0.1,
+			margin: 0.0,
 
 			// Bounds for smallest/largest possible scale to apply to content
-			minScale: 0.2,
-			maxScale: 1.0,
+			minScale: 0.0,
+			maxScale: 0.0,
 
 			// Display controls in the bottom right corner
-			controls: true,
+			controls: false,
 
 			// Display a presentation progress bar
-			progress: true,
+			progress: false,
 
 			// Display the page number of the current slide
 			slideNumber: false,
